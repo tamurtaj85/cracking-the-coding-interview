8137c2096c72050931387f1c47c27ea51e3940fe
Tamur Ahmed <tamurahmed@Tamurs-MacBook-Air.local>
2025-02-12 16:37:45 +0500

    dfs started

---
+++ 8 - Tree - Depth First Seach/1-pathSum.js
@@ -0,0 +1,12 @@
+/**
+ * Binary Tree Path Sum (easy)
+ *
+ * Given a binary tree and a number ‘S’, find if the tree has a path from root-to-leaf such that the sum of all the node values of that path equals ‘S’.
+ */
+class TreeNode {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
